7dd81e589bec8db2c373d5198998e737b5be7daa
Madelynn Cole <54558684+madecole@users.noreply.github.com>
2019-08-31 20:59:02 -0400

    Finishes Introduction

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ import utils
 
 utils.check_version((3,7))
 utils.clear()
-
-print('Hello, Class!')
+name = ''
+game = ''
+name = input("Hello there! What's your name?")
+print("Nice to meet you! I'm Maddie Cole!")
+print('What is my favorite game? I am currently obsessed with the Red Dead Redemption series.')
+print("What concerns do I have about this class? I'm a little worried about coding in python and I hope I can keep up and learn some new things.")
+print("I would have loved to know how to insert your name into the intro.. but maybe by next week!")
+print('StackOverflow #:12004508')
+print('GitHub URL: https://github.com/madecole')
